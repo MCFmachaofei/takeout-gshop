6f0d66ff4deada87f3b0ddbebe936db61586bd3f
machaofei <343964136@qq.com>
2018-05-16 19:08:43 +0800

    新增HeaderTop和ShopList

--- src/components/HeaderTop/HeaderTop.vue
+++ src/components/HeaderTop/HeaderTop.vue
@@ -1,12 +1,11 @@
 <template>
   <header class="header">
-    <slot name="search"></slot>
-    <a class="header_title">
-      <span class="header_title_text ellipsis">{{title}}</span>
-    </a>
-    <slot name="login"></slot>
+          <slot name="left"></slot>
+    <span class="header_title">
+            <span class="header_title_text ellipsis">{{title}}</span>
+          </span>
+    <slot name="right"></slot>
   </header>
-
 </template>
 <script>
   export default {
